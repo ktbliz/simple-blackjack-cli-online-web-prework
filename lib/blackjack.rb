7b1a6132b4c3060c9f96d0a08f1886bd742243b6
Katie Blizzard <kblizzard11@gmail.com>
2019-02-03 21:07:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,7 +51,7 @@ def hit?(card_total)
   input = get_user_input
   if input == "h"
     new_card = deal_card
-    puts new_card
+    # puts new_card
     card_total = new_card + card_total
     puts card_total
     return card_total
