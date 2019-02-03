77f88b5590d8c13bfd4672d190e6db4eb3f96b8e
Katie Blizzard <kblizzard11@gmail.com>
2019-02-03 21:12:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -74,9 +74,11 @@ def runner
   
   welcome 
   card_total = initial_round
-  card_total = hit?(card_total)
-  display_card_total(card_total)
-  end_game(card_total) if card_total > 21
+  while card_total < 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
   
 end
     
